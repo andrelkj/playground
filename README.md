ee8b418a16227c3bfa3c8b7b8da3694e63445ebb
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-10-27 01:45:10 -0300

    refactor: include before hooks to  login test suite

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ Once we start to write automated testing we need to stick with some best practic
 Checkpoints are verification points where you ensure the correct page is displayed or expected action took place prior to proceeding with any subsequent step:
 
 ```js
-    ...
     cy.visit("https://playground.cyskills.com.br");
 
     cy.contains("h2", "Faça login").should("be.visible"); // checkpoint to ensure the redirection to the login page
@@ -22,7 +21,65 @@ Checkpoints are verification points where you ensure the correct page is display
     cy.get('[data-cy="password"]').type("showtime");
 
     cy.get('[data-cy="login-button"]').click();
-    ...
 ```
 
 **Note:** we only proceed to the next steps if the checkpoint is confirmed.
+
+#### Hooks
+
+Hooks are a way to avoid repeating code, in the examples below we are defining the viewport for each test cases individually:
+
+```js
+  it("Should login successfully", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://playground.cyskills.com.br");
+
+    cy.contains("h2", "Faça login").should("be.visible");
+    ...
+    })
+
+  it("Should not login with invalid password", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://playground.cyskills.com.br");
+
+    cy.contains("h2", "Faça login").should("be.visible");
+    ...
+    })
+
+  it("Should not login with unresgistered email", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://playground.cyskills.com.br");
+
+    cy.contains("h2", "Faça login").should("be.visible");
+    ...
+    })
+
+  it("Should not login with invalid email", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://playground.cyskills.com.br");
+
+    cy.contains("h2", "Faça login").should("be.visible");
+    ...
+    })
+```
+
+A better approach is to use hook (e.g. `beforeEach` and `afterEach`) that will apply the action/behavior you want before or after each test cases, while also improving the code maintanance:
+
+```js
+describe("Login", () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit("https://playground.cyskills.com.br");
+
+        cy.contains("h2", "Faça login").should("be.visible");
+    });
+
+    it("Should login successfully", () => {
+        cy.get('[data-cy="email"]').type("papito@cyskills.com.br");
+        cy.get('[data-cy="password"]').type("showtime");
+        ...
+    })
+})
+```
+
+**Note:** in addition to `beforeEach` and `afterEach` you can also use the `before` and the `after` hooks to apply specific bahaviors one time only before or after all the test cases are executed.
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,13 @@
 describe("Login", () => {
-  it("Should login successfully", () => {
+  beforeEach(() => {
     cy.viewport(1920, 1080);
+
     cy.visit("https://playground.cyskills.com.br");
 
     cy.contains("h2", "Faça login").should("be.visible");
+  });
 
+  it("Should login successfully", () => {
     cy.get('[data-cy="email"]').type("papito@cyskills.com.br");
     cy.get('[data-cy="password"]').type("showtime");
 
@@ -16,79 +19,63 @@ describe("Login", () => {
   });
 
   it("Should not login with invalid password", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("https://playground.cyskills.com.br");
-
-    cy.contains("h2", "Faça login").should("be.visible");
-
     cy.get('[data-cy="email"]').type("papito@cyskills.com.br");
     cy.get('[data-cy="password"]').type("abc123456");
 
     cy.get('[data-cy="login-button"]').click();
 
-    cy.get('.notice p')
+    cy.get(".notice p")
       .should("be.visible")
-      .and("have.text", "E-mail ou senha incorretos. Por favor, tente novamente!");
+      .and(
+        "have.text",
+        "E-mail ou senha incorretos. Por favor, tente novamente!"
+      );
   });
 
   it("Should not login with unresgistered email", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("https://playground.cyskills.com.br");
-
-    cy.contains("h2", "Faça login").should("be.visible");
-
     cy.get('[data-cy="email"]').type("404@cyskills.com.br");
     cy.get('[data-cy="password"]').type("abc123456");
 
     cy.get('[data-cy="login-button"]').click();
 
-    cy.get('.notice p')
+    cy.get(".notice p")
       .should("be.visible")
-      .and("have.text", "E-mail ou senha incorretos. Por favor, tente novamente!");
+      .and(
+        "have.text",
+        "E-mail ou senha incorretos. Por favor, tente novamente!"
+      );
   });
 
   it("Should not login with invalid email", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("https://playground.cyskills.com.br");
-
-    cy.contains("h2", "Faça login").should("be.visible");
-
     cy.get('[data-cy="email"]').type("www.cyskills.com.br");
     cy.get('[data-cy="password"]').type("abc123456");
 
     cy.get('[data-cy="login-button"]').click();
 
-    cy.get('.notice p')
+    cy.get(".notice p")
       .should("be.visible")
-      .and("have.text", "O formato do e-mail está incorreto. Por favor, verifique e tente novamente!");
+      .and(
+        "have.text",
+        "O formato do e-mail está incorreto. Por favor, verifique e tente novamente!"
+      );
   });
 
   it("Should not login without email", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("https://playground.cyskills.com.br");
-
-    cy.contains("h2", "Faça login").should("be.visible");
-
     cy.get('[data-cy="password"]').type("abc123456");
 
     cy.get('[data-cy="login-button"]').click();
 
-    cy.get('.notice p')
+    cy.get(".notice p")
       .should("be.visible")
       .and("have.text", "Parece que você esqueceu de informar seu e-mail.");
   });
 
   it("Should not login without password", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("https://playground.cyskills.com.br");
-
-    cy.contains("h2", "Faça login").should("be.visible");
-
     cy.get('[data-cy="email"]').type("papito@cyskills.com.br");
 
     cy.get('[data-cy="login-button"]').click();
 
-    cy.get('.notice p')
+    cy.get(".notice p")
       .should("be.visible")
       .and("have.text", "Por favor, digite sua senha para continuar.");
   });
