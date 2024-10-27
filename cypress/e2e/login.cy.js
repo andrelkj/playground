c1afc8fbeb0b5ae84c87682ca6d5d19b548ec229
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-10-26 23:39:10 -0300

    test: add validation to successful login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,15 @@
-describe('Login', () => {
-  it('Should login successfully', () => {
-    cy.visit('https://playground.cyskills.com.br')
-  })
-})
+describe("Login", () => {
+  it("Should login successfully", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://playground.cyskills.com.br");
+
+    cy.get('[data-cy="email"]').type("papito@cyskills.com.br");
+    cy.get('[data-cy="password"]').type("showtime");
+
+    cy.get('[data-cy="login-button"]').click();
+
+    cy.get('[data-cy="welcome-title"]')
+      .should("be.visible")
+      .and("have.text", "Boas vindas ao Cypress Playground");
+  });
+});
