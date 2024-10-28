baaab666663f86c7a74b1647cca62d58cef3a22b
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-10-28 00:08:29 -0300

    test: add automation to fill textarea fields

---
+++ cypress/e2e/textarea.cy.js
@@ -0,0 +1,15 @@
+describe("Textarea", () => {
+  beforeEach(() => {
+    cy.goHome();
+  });
+
+  it("Should fill text area field", () => {
+    cy.login("papito@cyskills.com.br", "showtime");
+    cy.userLoggedIn();
+    cy.goTo("/textarea", "Textarea");
+
+    cy.get('textarea[name="message"]').type(
+      "Welcome to Cypress Skills. A complete course build for you to learn test automation with Cypress."
+    );
+  });
+});
