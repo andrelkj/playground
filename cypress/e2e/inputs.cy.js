cc4921a17720df1dc8db4ccb45916121c3320610
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-10-28 00:58:11 -0300

    refactor: update inputs test suite to use custom commands

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -7,12 +7,11 @@ describe("Input Fields", () => {
     cy.login("papito@cyskills.com.br", "showtime");
     cy.userLoggedIn();
 
-    cy.get('nav a[href="/input-fields"]').click();
-    cy.contains("h2", "Input Fields").should("be.visible");
+    cy.goTo("/input-fields", "Input Fields");
 
     cy.get('input[placeholder="John Doe"]').type("André Kreutzer");
-    cy.get('input[name="email"]').type("customer@test.com")
-    cy.get('[data-cy="number"]').type("123456")
-    cy.get('input[name="date"]').type("2024-10-27")
+    cy.get('input[name="email"]').type("customer@test.com");
+    cy.get('[data-cy="number"]').type("123456");
+    cy.get('input[name="date"]').type("2024-10-27");
   });
 });
