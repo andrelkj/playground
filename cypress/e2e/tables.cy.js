1317c3ca6b35c32eb09180220781b3328cfb0295
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-10-30 13:31:14 -0300

    test: include table item removal logic validation

--- cypress/e2e/tables.cy.js
+++ cypress/e2e/tables.cy.js
@@ -15,4 +15,22 @@ describe('Tables', () => {
       .and('contain', 'papitodev')
       .and('contain', 'Ativo')
   })
+
+  it('Should remove one social media', () => {
+    const name = 'Facebook'
+
+    cy.contains('table tbody tr', '1002').find('.remove-item').click()
+    cy.contains('button', 'Excluir').click()
+
+    cy.get('table tbody').should('not.contain', name)
+  })
+
+  it('Should social media should remain in the page after removal is canceled', () => {
+    const name = 'Youtube'
+
+    cy.contains('table tbody tr', '1005').find('.remove-item').click()
+    cy.contains('button', 'Cancelar').click()
+
+    cy.get('table tbody').should('contain', name)
+  })
 })
